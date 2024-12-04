997d35e4cc8e043f7578345e04f4fbcb2b390a83
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-04 07:44:27 +0200

    Add files via upload

---
+++ singers_loop.py
@@ -0,0 +1,4 @@
+singers = ["sakis", "elena", "nikos", "antonis"]
+for singer in singers:
+    print(f"{singer.title()}, that was a great show!")
+    print(f"I can't wait to see your next show,{singer.title()}.\n")
