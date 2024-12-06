89a2c3aec6521b9b22dd14f57c982f857534bbd7
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-06 22:33:26 +0200

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,6 @@
+pizzas = ["hawai", "alfredo", "special", "4 cheeses", "napoli", "crema mushroom"]
+
+for pizza in pizzas:
+    print(f"I like {pizza.title()}.\n")
+
+print("As you see, I really like pizza!")
