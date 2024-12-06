203ff5b4163216b1243cb9b83030f776dedc2e72
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-06 22:38:30 +0200

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,6 @@
+animals = ["dog", "cat", "rabbit", "dolphin", "rat", "iguana"]
+
+for animal in animals:
+    print(f"A {animal.title()} whould make a great pet!.\n")
+
+print("Any of these animals whould make a great pet!")
